da3abce4cc7176db64c04479e4844fa6d80d1ca3
Mario Yordanov <mario.yordanov@vitech.io>
2024-06-23 10:19:49 +0300

    first commit

---
+++ src/main/java/com/example/kotkogram/KotkogramApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotkogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotkogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotkogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kotkogram/KotkogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotkogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotkogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
